814615b97c27e330e4a7cc89b80b79b62046b10c
lamaslg <lamaslg@gmail.com>
2015-02-25 15:56:27 +0100

    PrimeraSubida

---
+++ DemoAjax/Ajax.js
@@ -0,0 +1,54 @@
+﻿function validarUsuario (login, pwd) {
+    //https://cursoesf.azure-mobile.net/tables/usuarios?$filter=login 
+    //eq 'luis' and password eq 'yo'
+    var request = new XMLHttpRequest();
+    var url = "https://cursoesf.azure-mobile.net/tables/usuarios";
+    var query = "?$filter=login eq '" + login + "' and password eq '" + pwd + "'";
+    request.open("get", url + query);
+
+    request.onreadystatechange = function () {
+
+        if (request.readyState == 4) {
+
+            if (request.status >= 200 && request.status < 300) {
+                var respuesta = request.responseText;
+
+                var respuestaObj = JSON.parse(respuesta);
+
+                if (respuestaObj.length > 0) {
+                    alert(respuestaObj[0].login);
+
+                } else {
+                    alert("Sin datos");
+                }
+
+
+            } else {
+                alert(request.error);
+            }
+
+
+        }
+
+
+    };
+
+
+    request.send(null);
+
+
+}
+(function() {
+    var bt = document.getElementById("btnValidar");
+
+    bt.addEventListener("click", function() {
+
+        var log = document.getElementById("txLogin").value;
+        var pwd = document.getElementById("txPassword").value;
+
+        validarUsuario(log, pwd);
+    });
+
+
+})();
+
---
+++ DemoAjax/Scripts/miweb.js
@@ -0,0 +1,90 @@
+﻿var url_base = "https://cursoesf.azure-mobile.net/tables";
+function validarUSuario() {
+
+    var log = $("#txLogin").val();
+    var pwd = $("#txPassword").val();
+
+    var url = url_base + "/usuarios";
+    var query = "?$filter=login eq '" + log + "' and password eq '" + pwd + "'";
+
+    $.getJSON(url + query, function(res) {
+        if (res.length > 0) {
+            listarDispositivos();
+            $("#listadoDispositivos").show(1500);
+            if (res[0].isAdmin) {
+                $("#crearDispositivo").show(1500);
+            }
+            $("#login").hide(1500);
+
+            //TODO: falta cargar los dispositivos actuales
+
+        } else {
+            alert("Datos incorrectos");
+        }
+
+
+    });
+}
+
+
+function crearDispositivo() {
+    var txNom = $("#txtNombre").val();
+    var txPre = $("#txtPrecio").val();
+
+    var obj = {
+        nombre: txNom,
+        precio: parseFloat(txPre)
+
+    };
+    var objtx = JSON.stringify(obj);
+    var url = url_base + "/dispositivos";
+
+   /* $.post(url, objtx, function(res) {
+        
+
+    }, "application/json");
+    */
+    $.ajax({
+        url: url,
+        data: objtx,
+        type: "post",
+        contentType: "application/json",
+        success: function(res) {
+            listarDispositivos();
+            alert("Dispositivo agregado");
+        }
+
+    });
+
+}
+
+function listarDispositivos() {
+
+    var tabla = $("#tbDispositivos");
+    tabla.empty();
+
+    var url = url_base + "/dispositivos";
+
+    $.getJSON(url, function(res) {
+        for (var i = 0; i < res.length; i++) {
+
+            var fila = "<tr>";
+            fila += "<td>" + res[i].nombre + "</td>";
+            fila += "<td>" + res[i].precio + "</td>";
+            fila += "</tr>";
+            tabla.append(fila);
+        }
+
+
+    });
+
+}
+
+$(document).ready(function() {
+    $("#btnValidar").click(validarUSuario);
+    $("#btnAdd").click(crearDispositivo);
+});
+
+
+
+
